154f314731ab0392b138fc935dc95f72a96c1347
OJ <onkarjoshi296@gmail.com>
2024-02-24 22:45:32 +0530

    added routes and controllers

--- src/app.js
+++ src/app.js
@@ -1,23 +1,24 @@
-import { express } from "express";
-import { DB_NAME } from "./constants.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-
-const app=express();
-
-app.use(core(
-    {
-        origin: process.env.CORS_ORIGIN,
-        credentials: true
-    }
-))
-app.use(express.json({
-    limit: "16kb"
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+const app = express()
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
 }))
-app.use(express.urlencoded({
-    extended: true, limit: "16kb"
-}))
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-export default app;
+
+//routes import
+import userRouter from './routes/user.routes.js'
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -1,51 +1,18 @@
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
+import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-import express  from "express";
-const app = express()
-
+import {app} from './app.js'
 dotenv.config({
-    path: "./env"
-});
-
-
+    path: './.env'
+})
 connectDB()
-.then(()=>{
-    const port = process.env.PORT || 8080;
-    app.listen(port, ()=>{
-        console.log(`Server is running on port: ${port}`);
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
-    app.on("error", (error)=>{
-        console.log("ERROR: ", error);
-        throw error
-    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
 
 
 
-//2nd method
-/*
-import {express } from "express";
-const app = express()
-
-//effy
-(async ()=>{
-    try {
-       await mongoose.connect(
-            `${process.env.MONGO_URI}/${DB_NAME}`,
-        )
-        app.on("error", (error)=>{
-            console.log("ERROR: ", error);
-            throw error
-        })
-
-        app.listen(process.env.PORT, ()=>{
-            console.log(`Server is running on port ${process.env.PORT}`);
-        })
-        
-    } catch (error) {
-        console.log("ERROR: ", error)
-        throw err
-    }
-})()*/
